990ebefb1d928e86cba7737f0bd413fa3c97be17
Mopahta <novoseletskiy03@mail.ru>
2022-04-12 00:45:21 +0300

    output without buffer

--- l3/task/task.c
+++ l3/task/task.c
@@ -3,14 +3,16 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <time.h>
+#include <string.h>
 
 void printCurrentTime(){
     struct timespec ts;
     timespec_get(&ts, TIME_UTC);
-    char buff[100];
-    strftime(buff, sizeof buff, "%F %T", gmtime(&ts.tv_sec));
-    fprintf(stdout, "Child process:\n\tPID %d\n\tPPID %d\nCurrent time: %s:%03ld UTC\n", getpid(), getppid(), buff, ts.tv_nsec/1000000); 
-    fflush(stdout); 
+    char bufftime[40], buf[100];
+    strftime(bufftime, sizeof bufftime, "%F %T", gmtime(&ts.tv_sec));
+    sprintf(buf, "Child process:\n\tPID %d\n\tPPID %d\nCurrent time: %s:%03ld UTC\n", getpid(), getppid(), bufftime, ts.tv_nsec/1000000); 
+    write(1, buf, strlen(buf));
+    exit(0);
 }
 
 int main(){
@@ -33,6 +35,5 @@ int main(){
     if (pid){
         system("ps -x");
     }
-    wait(NULL);
     return 0;
 }
